1d23a219f7baf7da33dcdd2a83668d892812b8f7
SCOPEterenceChan <dcywchan@cityu.edu.hk>
2023-07-21 21:05:39 +0800

    Add files via upload

---
+++ q4.py
@@ -0,0 +1,11 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:\Users\User\Downloads\WADD\PythonCodes\Triangle.py ========
+Please enter coordinate x1: 100
+Please enter coordinate y1: 10
+Please enter coordinate x2: 50
+Please enter coordinate y2: 100
+Please enter coordinate x3: 100
+Please enter coordinate y3: 100
+Area of your triangle is 2250.0
